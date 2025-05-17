a83b0645ff4164e8d188866a4a54c547c5dd4f3b
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-05-17 09:00:27 +0900

    2 lv.3 problem

--- Lv.3/아이템_줍기.js
+++ Lv.3/아이템_줍기.js
@@ -1,33 +1,62 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    let board = Array.from({length: 12}, () => Array(12).fill(0));
+    let board = Array.from({length: 102}, () => Array(102).fill(0));
+    let visted = Array.from({length: 102}, () => Array(102).fill(false));
+    let distance = Array.from({length: 102}, () => Array(102).fill(0));
+
     for(square of rectangle){
         fillSquare(square, board);
     }
+    for(square of rectangle){
+        removeExceptEdges(square, board);
+    }
 
     function fillSquare(coordinates, arr){
-        for(let i=coordinates[1]; i<= coordinates[3]; i++){
-            for(let j=coordinates[0]; j<=coordinates[2]; j++){
+        const [stX, stY, lsX, lsY] = coordinates;
+        for(let i=stY*2; i<= lsY*2; i++){
+            for(let j=stX*2; j<=lsX*2; j++){
                 arr[i][j] = 1;
             }
         }
     }
     
     function removeExceptEdges(coordinates, arr){
-        for(let i=coordinates[1]+1; i<= coordinates[3]-1; i++){
-            for(let j=coordinates[0]+1; j<=coordinates[2]-1; j++){
+        const [stX, stY, lsX, lsY] = coordinates;
+        for(let i=stY*2+1; i<=  lsY*2-1; i++){
+            for(let j=stX*2+1; j<=lsX*2-1; j++){
                 arr[i][j] = 0;
             }
         }
     }
     
-    board.forEach(row => console.log(row.join(' ')));
-    return null;
+    const dy = [0, 0, 1, -1];
+    const dx = [1, -1, 0, 0];
+    bfs(characterY*2, characterX*2);
+
+    function bfs(startY, startX){
+        const queue = [];
+        queue.push([startY, startX]);
+        visted[startY][startX] = true;
+        distance[startY][startX] = 0;
+
+        while(queue.length){
+            const[currY, currX] = queue.shift();
+
+            for(let d=0; d < 4; d++){
+                const nextY = currY + dy[d];
+                const nextX = currX + dx[d];
+                const isYInBoard =  (0 <= nextY && nextY < 24);
+                const isXInBoard =  (0 <= nextX && nextX < 24);
+                if(isYInBoard && isXInBoard && 
+                    board[nextY][nextX] == 1 && !visted[nextY][nextX]){
+                    queue.push([nextY, nextX]);
+                    visted[nextY][nextX] = true;
+                    distance[nextY][nextX] = distance[currY][currX] + 1;
+                }
+            }
+        }
+    }
+
+
+    const shortestRoute = distance[itemY*2][itemX*2];
+    return shortestRoute/2;
 }
-
-const rectangle = [[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]];
-const characterX = 1;
-const characterY = 3;
-const itemX = 7;
-const itemY = 8;
-
-solution(rectangle, characterX, characterY, itemX, itemY);
